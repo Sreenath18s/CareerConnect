f7bcbf1d88821c343fa313244197966c9cd717a6
Rithish <sivasanthur1234@gmail.com>
2023-11-01 15:08:00 +0530

    favicon

--- assets/mail.php
+++ assets/mail.php
@@ -1,114 +1,37 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = trim($_POST["name"]);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $subject = trim($_POST["subject"]);
+    $message = trim($_POST["message"]);
 
-
-
-    // Only process POST reqeusts.
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        // Get the form fields and remove MORALspace.
-
-        $name = strip_tags(trim($_POST["name"]));
-
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
-
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-
-        $subject = trim($_POST["subject"]);
-
-        $message = trim($_POST["message"]);
-
-
-
-        // Check that data was sent to the mailer.
-
-        if ( empty($name) OR empty($subject) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-
-            // Set a 400 (bad request) response code and exit.
-
-            http_response_code(400);
-
-            echo "Please complete the form and try again.";
-
-            exit;
-
-        }
-
-
-
-        // Set the recipient email address.
-
-        // FIXME: Update this to your desired email address.
-
-        $recipient = "careerconnectmp@gmail.com";
-
-
-
-        // Set the email of sender.
-
-        $sender = "New contact from $name";
-
-
-
-        //Email Header
-
-        $head = " ====== BASIC THEME ====== ";
-
-
-
-        // Build the email content.
-
-        $email_content = "$head\n\n\n";
-
-        $email_content .= "Name: $name\n";
-
-        $email_content .= "Email: $email\n\n";
-
-        $email_content .= "Subject: $subject\n\n";
-
-        $email_content .= "Message:\n$message\n";
-
-
-
-        // Build the email headers.
-
-        $email_headers = "From: $name <$email>";
-
-
-
-        // Send the email.
-
-        if (mail($recipient, $sender, $email_content, $email_headers)) {
-
-            // Set a 200 (okay) response code.
-
-            http_response_code(200);
-
-            echo "Thank You! Your message has been sent.";
-
-        } else {
-
-            // Set a 500 (internal server error) response code.
-
-            http_response_code(500);
-
-            echo "Oops! Something went wrong and we couldn't send your message.";
-
-        }
-
-
-
-    } else {
-
-        // Not a POST request, set a 403 (forbidden) response code.
-
-        http_response_code(403);
-
-        echo "There was a problem with your submission, please try again.";
-
+    if (empty($name) || empty($subject) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        http_response_code(400);
+        echo "Please complete the form correctly and try again.";
+        exit;
     }
 
+    $recipient = "careerconnectmp@gmail.com"; // Replace with the actual recipient's email address
+    $sender = $name;
+    $subject = "Contact Form Submission: $subject";
 
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Subject: $subject\n\n";
+    $email_content .= "Message:\n$message\n";
 
+    $email_headers = "From: $email\r\n";
+    $email_headers .= "Reply-To: $email\r\n";
+
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        http_response_code(200);
+        echo "Thank you! Your message has been sent.";
+    } else {
+        http_response_code(500);
+        echo "Oops! Something went wrong, and we couldn't send your message.";
+    }
+} else {
+    http_response_code(403);
+    echo "There was a problem with your submission. Please try again.";
+}
 ?>
-
